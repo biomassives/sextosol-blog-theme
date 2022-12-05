a20ddd04d539a04e2eb5f15f3b007b3154976cf1
G. Willson <greg@biomassiv.es>
2022-12-05 13:54:09 -0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,14 +4,17 @@
 <div class="container">
 <div class="row justify-content-between">
 <div class="col">
+
+<div style="color: #000; border-radius: 25px; background: #fff; background-position: left top; background-repeat: repeat; padding: 20px; width: 200px; height: 150px;">
+
+
 <a href="/"><img class="logofooter" width="322" height="37" :src="($withBase)($themeConfig.logo)"></a>
 
 
-<BR /><BR />
-
-<div style="color: #000; border-radius: 25px; background: #fff; background-position: left top; background-repeat: repeat; padding: 20px; width: 200px; height: 150px;">
+<BR /> Please consider donating monthly. <BR />
+   
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-    <br>$ <font face="helvetica"><input type="textfield" name="a3" value="" size="10"> **Enter amount and number of months</font>
+    <br>$ <font face="helvetica"><input type="textfield" name="a3" value="" size="10"> *Enter amount and number of months</font>
     <font size="2" face="helvetica">
     for <input type="text fields" name="Number_of_Months" value="12" size="2"> months*<br><br></font><font face="helvetica">
     <input type="hidden" name="p3" value="1">
